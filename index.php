9943f7e2b0aed7b95b38382c4be0fe1c3ed6a8fe
Qumusu <kei2ano333@gmail.com>
2018-09-02 00:16:55 +0900

    データ出力

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
-
-mysqli_connect("localhost","root","root");
+$link = mysqli_connect("localhost","root","root","memberapp");
 // サーバー名、データベース名、パスワード
 
 if(mysqli_connect_error()){
-    echo "データベースへの接続に失敗しました。";
-}else{
+    die("データベースへの接続に失敗しました。");
+}
+
+$query = "SELECT * FROM users";
+
+if($result = mysqli_query($link,$query)){
+    echo "クエリの発行に成功しました";
+}
+$row = mysqli_fetch_array($result);
+
+//print_r($row);
+
+echo "あなたのメールアドレスは".$row['email']."、パスワードは".$row['password']."です。";
+
+echo "<p>";
+
+echo "あなたのメールアドレスは".$row['1']."、パスワードは".$row['2']."です。";
+
+echo "<p>";
+
+echo "あなたのIDは".$row['id']."です。";
+
+/*else{
     echo "データベースへの接続に成功しました。";
 };
-
+*/
 ?>
