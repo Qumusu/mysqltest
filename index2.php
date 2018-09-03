1816b20ff90b322c0e9269ed031ad7a32ebcae19
Qumusu <kei2ano333@gmail.com>
2018-09-03 23:55:33 +0900

    メール

---
+++ index2.php
@@ -0,0 +1,50 @@
+<?php
+    $link = mysqli_connect("localhost","root","root","memberapp");
+    // サーバー名、データーベースユーザ名、パスワード
+    
+    if(mysqli_connect_error()){
+        die("データベースの接続に失敗しました。");
+    }
+    
+    //$name = "Rob O'Grady";
+    //$query = "SELECT = FROM users WHERE name ='".mysqli_real_escape_string($link,$name)."'";
+
+    if(array_key_exists('email',$_POST) OR array_key_exists('password',$_POST)){
+        if($_POST['email'] == ''){
+            echo "Eメールアドレスを入力してください";
+        }elseif($_POST['password'] == ''){
+            echo "パスワードを入力してください";
+        }else {
+            $query = "SELECT 'id' FROM 'users' WHERE email ='".mysqli_real_escape_string($link,$_POST['email'])."'";
+            $result = mysqli_query($link,$query);
+            if(mysqli_num_rows($result) > 0){
+                echo "すでにそのメールアドレスは使用されています。";
+            } else {
+                // 未使用の場合の処理を記述
+                $query = "INSERT INTO 'users' ('email','password') VALUES ('".mysqli_real_escape_string($link,$_POST['email'])."','".mysql_real_escape_string($link,$_POST['password'])."')";
+                if(mysqli_query($link,$query)){
+                    echo "登録されました";
+                }else {
+                    echo "登録に失敗しました";
+                }
+            }
+        }
+    }
+
+/*
+
+1. Eメールとパスワードの入力フォーム、「登録する」ボタンを設置する
+2. データが入力されているかどうかチェクする
+3. メールアドレスが既に使用されていないかチェックする
+4. 重複がなければユーザ登録を実行する
+5. ユーザ登録に成功しました、というメッセージを表示する。
+
+*/
+
+?>
+
+<form method="post">
+    <input name="email" type="text" placeholder="Eメール">
+    <input name="password" type="password" placeholder="パスワード">
+    <input type="submit" value="登録する">
+</form>
